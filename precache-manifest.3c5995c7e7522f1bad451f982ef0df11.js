32b14df4cfe0cdcabb0b0ff6169fc9606230b8c1
makkiboukhris <makki01boukhris@gmail.com>
2020-09-18 12:44:46 +0100

    Updates

---
+++ precache-manifest.3c5995c7e7522f1bad451f982ef0df11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae5c78e0a06444d590ccef74710fdee",
+    "url": "/components-gh-pages/index.html"
+  },
+  {
+    "revision": "8afa7d06ec0965ba0aa8",
+    "url": "/components-gh-pages/static/css/main.ea7be399.chunk.css"
+  },
+  {
+    "revision": "da66910e5eea170fc313",
+    "url": "/components-gh-pages/static/js/2.32e62788.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/components-gh-pages/static/js/2.32e62788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8afa7d06ec0965ba0aa8",
+    "url": "/components-gh-pages/static/js/main.a08a7f74.chunk.js"
+  },
+  {
+    "revision": "6a9e8af3a89e5589dfbb",
+    "url": "/components-gh-pages/static/js/runtime-main.c5b14e72.js"
+  }
+]);
